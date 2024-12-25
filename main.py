c780ef219026928f59d4b38cf8781b63efd91d48
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2024-12-26 04:16:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -154,10 +154,11 @@ async def upload(bot: Client, m: Message):
 
             elif '/360p.zip' in url:
                 parts = url.split("/")
-                video_id = parts[-3]
-                zip_file_name = parts[-1]
+                video_id = parts[-3]  # Extract the video ID
+                zip_file_name = parts[-1]  # Get the ZIP file name
                 download_url = f"https://appx-transcoded-videos-mcdn.akamai.net.in/videos/ssbguide-data/{video_id}/video.mp4"
-                return download_url
+             return download_url
+
 
     elif url.endswith((".mp4", ".mkv")):
         return url
